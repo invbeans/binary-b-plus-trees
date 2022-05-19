06fdf52e7e06696bdd231a02b20b2e980ea30e13
strwbr <borisova25122001@gmail.com>
2022-05-20 01:31:55 +0400

    Create BinaryTree

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
+import org.example.model.BinaryTree;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        testBinaryTree(new BinaryTree());
+    }
+
+    // генератор массива
+    public static int[] generateKeyArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * 80); // [0;80]
+        }
+        return arr;
+    }
+
+    public static void testBinaryTree(BinaryTree binaryTree) {
+        int n = 12;
+        //int[] arr = {10, 35, 15, 17, 20, 27, 24, 8, 30}; // дерево из методички
+        int[] arr = {18, 71, 39, 47, 25, 77, 56, 67, 6, 35, 73, 0};
+        //int[] arr = generateKeyArray(n);
+        System.out.println("Отсортированный массив");
+        System.out.println(Arrays.toString(arr));
+
+        binaryTree.createTree(arr);
+        System.out.println("Построенное двоичное дерево:");
+        binaryTree.print();
+
+        System.out.println("Симметричный обход");
+        binaryTree.inOrder(binaryTree.getRoot());
+
+        int num = 11;
+        System.out.println("\nПоиск по близости сверху");
+        System.out.println("Результат поиска числа " + num + ": " + binaryTree.searchBigger(num));
     }
 }
--- src/main/java/org/example/model/node/BinaryNode.java
+++ src/main/java/org/example/model/node/BinaryNode.java
@@ -1,4 +1,121 @@
 package org.example.model.node;
 
 public class BinaryNode {
+    public int key;
+    public BinaryNode left;
+    public BinaryNode right;
+
+    public BinaryNode(int key, BinaryNode left, BinaryNode right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+    }
+
+    // поиск по близости сверху
+    public BinaryNode searchBigger(int num) {
+        if (this.key == num) return this;
+        if (this.left == null && this.right == null) return this;
+
+        BinaryNode tmpLeft, tmpRight;
+
+        // если у узла есть только правый ребенок
+        if (this.left == null) {
+            tmpRight = this.right.searchBigger(num);
+
+            if (tmpRight.key == num) return right;
+            int dif1 = num - tmpRight.key;
+            int dif2 = num - this.key;
+
+            if (Math.abs(dif1) < Math.abs(dif2)) {
+                if (dif1 < 0) return tmpRight;
+                else {
+                    if (dif2 < 0) return this;
+                    else return tmpRight;
+                }
+            } else {
+                if (dif2 < 0) return this;
+                else {
+                    if (dif1 < 0) return tmpRight;
+                    else return this;
+                }
+            }
+        }
+
+        // если у узла есть только левый ребенок
+        if (this.right == null) {
+            tmpLeft = this.left.searchBigger(num);
+
+            if (tmpLeft.key == num) return right;
+            int dif1 = num - tmpLeft.key;
+            int dif2 = num - this.key;
+            if (Math.abs(dif1) < Math.abs(dif2)) {
+                if (dif1 < 0) return tmpLeft;
+                else {
+                    if (dif2 < 0) return this;
+                    else return tmpLeft;
+                }
+            } else {
+                if (dif2 < 0) return this;
+                else {
+                    if (dif1 < 0) return tmpLeft;
+                    else return this;
+                }
+            }
+        }
+
+        // если у узла есть оба ребенка
+        tmpLeft = this.left.searchBigger(num);
+        if (tmpLeft.key == num) return tmpLeft;
+        tmpRight = this.right.searchBigger(num);
+        if (tmpRight.key == num) return tmpRight;
+
+        int dif1 = num - tmpLeft.key;
+        int dif2 = num - tmpRight.key;
+        int dif3 = num - this.key;
+        BinaryNode tmpMin = null;
+        if (Math.abs(dif1) < Math.abs(dif2) && Math.abs(dif1) < Math.abs(dif3)) {
+            tmpMin = tmpLeft;
+            if (dif1 < 0) return tmpLeft;
+            else {
+                if (dif2 < 0) {
+                    if (dif3 < 0) {
+                        if (dif2 > dif3) return tmpRight;
+                        else return this;
+                    } else return tmpRight;
+                } else {
+                    if (dif3 < 0) return this;
+                    else return tmpLeft;
+                }
+            }
+        } else if (Math.abs(dif2) < Math.abs(dif1) && Math.abs(dif2) < Math.abs(dif3)) {
+            tmpMin = tmpRight;
+            if (dif2 < 0) return tmpRight;
+            else {
+                if (dif1 < 0) {
+                    if (dif3 < 0) {
+                        if (dif1 > dif3) return tmpLeft;
+                        else return this;
+                    } else return tmpLeft;
+                } else {
+                    if (dif3 < 0) return this;
+                    else return tmpRight;
+                }
+            }
+        } else if (Math.abs(dif3) < Math.abs(dif1) && Math.abs(dif3) < Math.abs(dif2)) {
+            tmpMin = this;
+            if (dif3 < 0) return this;
+            else {
+                if (dif1 < 0) {
+                    if (dif2 < 0) {
+                        if (dif1 > dif2) return tmpLeft;
+                        else return tmpRight;
+                    } else return tmpLeft;
+                } else {
+                    if (dif2 < 0) return tmpRight;
+                    else return this;
+                }
+            }
+        }
+        return tmpMin;
+    }
 }
