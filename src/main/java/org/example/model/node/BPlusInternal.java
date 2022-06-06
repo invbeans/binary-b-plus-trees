3ce411978ea1d48b54973b46312e1560644d07ad
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-06-06 22:59:16 +0400

    bplus tree

--- src/main/java/org/example/model/node/BPlusInternal.java
+++ src/main/java/org/example/model/node/BPlusInternal.java
@@ -15,47 +15,51 @@ public class BPlusInternal extends BPlusNode {
     BPlusNode[] pointers;
     BPlusInternal right;
     BPlusInternal left;
-    public BPlusInternal(int level, Integer[] key){
+
+    public BPlusInternal(int level, Integer[] key) {
         this.maxLevel = level;
-        this.minLevel = (int)Math.ceil(level/2.0);
+        this.minLevel = (int) Math.ceil(level / 2.0);
         this.curLevel = 0;
         this.key = key;
-        this.pointers = new BPlusNode[this.maxLevel+1];
+        this.pointers = new BPlusNode[this.maxLevel + 1];
     }
-    public BPlusInternal(int level, Integer[] key, BPlusNode[] pointers){
+
+    public BPlusInternal(int level, Integer[] key, BPlusNode[] pointers) {
         this.maxLevel = level;
-        this.minLevel = (int)Math.ceil(level/2.0);
+        this.minLevel = (int) Math.ceil(level / 2.0);
         this.curLevel = linearNullSearch(pointers);
         this.key = key;
         this.pointers = pointers;
     }
 
-    public int linearNullSearch(BPlusNode[] arr){
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == null) return i;
+    public int linearNullSearch(BPlusNode[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null)
+                return i;
         }
         return -1;
     }
 
-    public void removePointer(int index){
+    public void removePointer(int index) {
         this.pointers[index] = null;
         this.curLevel--;
     }
 
-    public int findIndexOfPointer(BPlusNode pointer){
-        for(int i = 0; i < this.pointers.length; i++){
-            if(this.pointers[i] == pointer) return i;
+    public int findIndexOfPointer(BPlusNode pointer) {
+        for (int i = 0; i < this.pointers.length; i++) {
+            if (this.pointers[i] == pointer)
+                return i;
         }
         return -1;
     }
 
-    public void appendChildPointer(BPlusNode pointer){
+    public void appendChildPointer(BPlusNode pointer) {
         this.pointers[curLevel] = pointer;
         this.curLevel++;
     }
 
-    public void insertChildPointer(BPlusNode pointer, int index){
-        for(int i = curLevel - 1; i >= index; i--){
+    public void insertChildPointer(BPlusNode pointer, int index) {
+        for (int i = curLevel - 1; i >= index; i--) {
             this.pointers[i + 1] = this.pointers[i];
         }
         this.pointers[index] = pointer;
--- src/main/java/org/example/model/node/BPlusLeaf.java
+++ src/main/java/org/example/model/node/BPlusLeaf.java
@@ -15,33 +15,36 @@ public class BPlusLeaf extends BPlusNode {
     BPlusLeaf right;
     Integer[] key;
 
-    public BPlusLeaf(int level, int elem){
+    public BPlusLeaf(int level, int elem) {
         this.maxNum = level - 1;
-        this.minNum = (int)(Math.ceil(level/2.0)-1);
+        this.minNum = (int) (Math.ceil(level / 2.0) - 1);
         this.key = new Integer[level];
         this.curNum = 0;
         this.addElem(elem);
     }
 
-    public BPlusLeaf(int level, Integer[] key, BPlusInternal parent){
+    public BPlusLeaf(int level, Integer[] key, BPlusInternal parent) {
         this.maxNum = level - 1;
-        this.minNum = (int)(Math.ceil(level/2.0)-1);
+        this.minNum = (int) (Math.ceil(level / 2.0) - 1);
         this.key = key;
         this.curNum = linearNullSearch(key);
         this.parent = parent;
     }
 
-    public int linearNullSearch(Integer[] arr){
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == null) return i;
+    public int linearNullSearch(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null)
+                return i;
         }
         return -1;
     }
 
-    public boolean addElem(int elem){
-        if(this.curNum == this.maxNum){ return false; }
-        else{
-            if(this.key == null) this.key = new Integer[1];
+    public boolean addElem(int elem) {
+        if (this.curNum == this.maxNum) {
+            return false;
+        } else {
+            if (this.key == null)
+                this.key = new Integer[1];
             this.key[this.curNum] = elem;
             this.curNum++;
             Arrays.sort(this.key, 0, this.curNum);
@@ -49,7 +52,7 @@ public class BPlusLeaf extends BPlusNode {
         }
     }
 
-    public void deleteElem(int index){
+    public void deleteElem(int index) {
         this.key[index] = null;
         this.curNum--;
     }
